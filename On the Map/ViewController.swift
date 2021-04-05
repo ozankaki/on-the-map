d78a3f4c6bc47a8a9f2d8e5f4bff56a502f43397
Ozan Kaki <ozankaki@gmail.com>
2021-04-06 00:41:28 +0300

    Initial Commit

---
+++ On the Map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  On the Map
+//
+//  Created by Ozan Kaki on 6.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
